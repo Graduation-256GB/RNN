2fcf1e0b4d71552a380628e76ec494eb304595be
Sungeun <sungeuni0208@naver.com>
2021-08-01 23:57:43 +0900

    fix rnn_test

--- rnn_test.py
+++ rnn_test.py
@@ -6,9 +6,12 @@ import pandas as pd
 
 # 7.20 데이터 로드 및 확인
 # 데이터를 메모리에 불러옵니다. encoding 형식으로 utf-8 을 지정해야합니다.
-train_text = open('./dataset/pose256GB_20.csv', 'rb').read().decode(encoding='utf-8')
+train_text = open('./dataset/jsontocsv.csv', 'rb').read().decode(encoding='utf-8')
 # train_text = open(path_to_train_file, 'rb').read().decode(encoding='utf-8')
-test_text = open('./dataset/test_pose256GB.csv', 'rb').read().decode(encoding='utf-8')
+test_text = open('./dataset/jsontocsv.csv', 'rb').read().decode(encoding='utf-8')
+
+# 연속 프레임 개수
+n_steps=16
 
 # 텍스트가 총 몇 자인지 확인합니다.
 print('Length of text: {} characters'.format(len(train_text)))
@@ -17,14 +20,13 @@ print()
 
 # 7.21 학습을 위한 정답 데이터(Y) 만들기
 #### 수정 -> 20개의 데이터 당 하나의 y값을 주기 위해서 바꿈 [[1] [0]]
-train_Y = [[row.split(',')[15]] for row in train_text.split('\r')[19::20]]
-test_Y = [[row.split(',')[15]] for row in test_text.split('\r')[19::20]]
+train_Y = [[row.split(',')[15]] for row in train_text.split('\r')[15::16]]
+test_Y = [[row.split(',')[15]] for row in test_text.split('\r')[15::16]]
 
 # 운동 이름을 정수로 변경
 train_ylist=[]
 for y_train in train_Y:
     for y in y_train:
-        y=y.replace('squat','0').replace('running','1')
         train_ylist.append([int(y)])
 
 train_Y=np.array(train_ylist)
@@ -32,10 +34,11 @@ train_Y=np.array(train_ylist)
 test_ylist=[]
 for y_test in test_Y:
     for y in y_test:
-        y=y.replace('squat','0').replace('running','1')
         test_ylist.append([int(y)])
 
 test_Y=np.array(test_ylist)
+
+class_num=len(np.unique(train_Y))
 
 print("여기까지 ok")
 print(train_Y)
@@ -45,13 +48,16 @@ print(train_Y.shape, test_Y.shape)
 # 7.22 train 데이터의 입력(X)에 대한 정제(Cleaning)
 #### 수정 -> 줄바꿈 기호 없애고 빈 문자가 아닌 경우에만 데이터에 추가
 train_xlist=[]
-for row in train_text.split('\n')[0:]:
+train_x=train_text.split('\n')[0:]
+blocks=int(len(train_x)/n_steps)
+
+for row in train_x:
     list=row.split(',')[0:15]
     for i in list:
         if i!='':
             train_xlist.append(i)
 train_X=np.array(train_xlist)
-train_X=train_X.reshape(2,20,15).astype(float)
+train_X=train_X.reshape(blocks,n_steps,15).astype(float)
 
 test_xlist=[]
 for row in test_text.split('\n')[0:]:
@@ -60,7 +66,7 @@ for row in test_text.split('\n')[0:]:
         if i!='':
             test_xlist.append(i)
 test_X=np.array(test_xlist)
-test_X=test_X.reshape(2,20,15).astype(float)
+test_X=test_X.reshape(blocks,n_steps,15).astype(float)
 
 
 print("X 확인")
@@ -79,9 +85,9 @@ model = tf.keras.Sequential([
     #tf.keras.layers.Dropout(0.5),
     #tf.keras.layers.MaxPooling1D(pool_size=4),
 
-    tf.keras.layers.SimpleRNN(units=15, activation='tanh', return_sequences=True, input_shape=[20,15]),
+    tf.keras.layers.SimpleRNN(units=15, activation='tanh', return_sequences=True, input_shape=[n_steps,15]),
     tf.keras.layers.LSTM(units=10),
-    tf.keras.layers.Dense(2, activation='softmax')
+    tf.keras.layers.Dense(class_num, activation='softmax')
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
